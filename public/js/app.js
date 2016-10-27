7bdd734762dea07fe65a6b247e7650f3b536bc86
Eliot Slevin <Eliot.slevin@gmail.com>
2016-10-27 19:31:08 +1300

    render update

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,4 @@
-var data = {
-	medications:[
-	    {name: "Medicine Name", brandName:"Metchek", times:{w:0,b:1,l:0,d:1,bt:0}},
-	    {name: "Medicine Name", brandName:"Metchek", times:{w:0,b:1,l:0,d:1,bt:0}},
-	    {name: "Medicine Name", brandName:"Metchek", times:{w:0,b:1,l:0,d:1,bt:0}},
-	    {name: "Medicine Name", brandName:"Metchek", times:{w:0,b:1,l:0,d:1,bt:0}},
-	    {name: "Medicine Name", brandName:"Metchek", times:{w:0,b:1,l:0,d:1,bt:0}},
-	]
-}
-
+//Configuring firebase database connection
 var config = {
  apiKey: "AIzaSyAkMPKIfxCSg8aaCGz6Mq6AQYvtfAMufxM",
  authDomain: "medipro-d5254.firebaseapp.com",
@@ -18,20 +9,51 @@ var config = {
 
 firebase.initializeApp(config);
 
+// Render app on every change
 var dbRef = firebase.database().ref('/');
 dbRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
+  render(snapshot.val());
 });
-
-
-
-
-
 render();
 
-function render(){
+function bootstrapData(){
+	firebase.database().ref('/').set({
+		name: 'Ngaio Smith',
+
+		medications:[
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: true, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		],
+	});
+}
+
+function handleTaken(el){
+	var element = $(el);
+	var value = element.is(':checked');
+	var index = element.attr('id')
+
+	// Save to database
+
+	firebase.database().ref('/medications').set({
+		name: 'Ngaio Smith',
+
+		medications:[
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: true, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		    {name: "Medicine Name", brandName:"Metchek", isTaken: false, times:{w:0,b:1,l:0,d:1,bt:0}},
+		],
+	});
+}
+
+
+function render(snapData){
 	var source = $('#medicine-template').html();
 	var template = Handlebars.compile(source);
-	var html = template(data); 
+	var html = template(snapData); 
 	$('.medication-list').html(html);
 }
